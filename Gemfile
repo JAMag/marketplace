ec60052ca3ce43d509aa45c16c94d3bf8df6e138
Justin Maguire <maguirejustin@hotmail.com>
2017-03-11 11:58:10 -0500

    Added states to sales

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'dotenv-rails', groups: [:development]
 gem 'aws-sdk', '< 2.0 '
 gem 'friendly_id'
 gem 'stripe'
+gem 'aasm'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,6 +2,27 @@ class Sale < ActiveRecord::Base
   before_create :populate_guid
   belongs_to :book
 
+  include AASM
+
+  aasm column: 'state' do
+    state :pending, initial: true
+    state :processing
+    state :finished
+    state :errored
+
+    event :process, after: :charge_card do
+      transitions from: :pending, to: :processing
+    end
+
+    event :finish do
+      transitions from: :processing, to: :finished
+    end
+
+    event :fail do
+      transitions from: :processing, to: :errored
+    end
+  end
+
   private
   def populate_guid
     self.guid = SecureRandom.uuid()
